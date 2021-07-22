0d09df8d3e078aae539cc1573fc1f4636af19b3b
Timo Celik <timo.celik@gmail.com>
2021-07-22 06:48:20 +0200

    initial version

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.sass";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+function VisualPizza95({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default VisualPizza95;
